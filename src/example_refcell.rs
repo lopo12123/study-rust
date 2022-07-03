df5e5df187fa40652dfe322f52e2856ef6bf950e
lopo <lopo@zju.edu.cn>
2022-07-03 22:19:55 +0800

    example: refcell

---
+++ src/example_refcell.rs
@@ -0,0 +1,49 @@
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: 'a + Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+    where T: Messenger {
+    pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, msg: &str) {
+            // 使用 borrow_mut 方法获取可变借用
+            self.sent_messages.borrow_mut().push(String::from(msg));
+        }
+    }
+}
