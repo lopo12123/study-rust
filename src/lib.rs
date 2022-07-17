700653c26312f3ef483b13e17c3c5c9e92817c47
lopo <lopo@zju.edu.cn>
2022-07-17 20:54:12 +0800

    project: a web server

---
+++ src/lib.rs
@@ -0,0 +1,103 @@
+use std::sync::{Arc, mpsc, Mutex};
+use std::thread;
+
+// region call in box
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<Self>) {
+        (*self)();
+    }
+}
+// endregion
+
+// region worker
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = Some(thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} Got a job, executing...", id);
+
+                    job.call_box();
+                }
+                Message::Terminate => {
+                    println!("Worker {} is going to terminate...", id);
+                    break;
+                }
+            }
+        }));
+        Worker { id, thread }
+    }
+}
+// endregion
+
+// region job
+type Job = Box<dyn FnBox + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+// endregion
+
+// thread-pool
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool
+    ///
+    /// The size is the max number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero or lower.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, receiver.clone()));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+        where F: FnOnce() + Send + 'static {
+        let job = Box::new(f);
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for _ in &mut self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+        println!("Shutting down all workers...");
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}...", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+// endregion
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,48 @@
-use std::{env, process};
-use study_rust::{QueryConfig, run};
+use std::fs;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use study_rust::ThreadPool;
 
 fn main() {
-    let args = env::args().collect();
-    let config = QueryConfig::new(args).unwrap_or_else(|err| {
-        eprintln!("{}", err);
-        process::exit(1);
-    });
+    let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
+    let pool = ThreadPool::new(4);
 
-    if let Err(err) = run(config) {
-        eprintln!("error when reading file: {:?}", err);
-        process::exit(1);
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    // 请求
+    // Method Request-URI HTTP-Version CRLF
+    // headers CRLF
+    // message-body
+
+    // 响应
+    // !注意: 浏览器安全策略, 响应头需要有 Content-Length 字段才能被正确接收
+    // HTTP-Version Status-Code Reason-Phrase CRLF
+    // headers CRLF
+    // message-body
+
+    if buffer.starts_with(b"GET / HTTP/1.1\r\n") {
+        let contents = fs::read_to_string("./pages/index.html").unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let contents = fs::read_to_string("./pages/404.html").unwrap();
+        let response = format!("HTTP/1.1 404 NOT FOUND\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
     }
 }
